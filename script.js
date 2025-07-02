0fbb29003aa28298827a536575e22e1d90f268f6
MathDev96 <camilooliveiradev@gmail.com>
2025-07-02 15:35:14 -0300

    Adiciona novas imagens de produtos e implementa carrossel na seção hero, além de melhorias no layout e estilo da página inicial

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("ano").textContent = anoAtual;
 });
 
+// Carrossel de imagens produtos
 document.addEventListener('DOMContentLoaded', function () {
     const carrosseis = document.querySelectorAll('.carrossel');
 
@@ -21,5 +22,66 @@ document.addEventListener('DOMContentLoaded', function () {
             imagens[index].classList.add('ativo');
         }, 5000);
     });
+});
+
+// Carrossel de imagens na seção hero
+document.addEventListener('DOMContentLoaded', function () {
+    const images = document.querySelectorAll('.hero-image img');
+    const leftBtn = document.querySelector('.arrow.left');
+    const rightBtn = document.querySelector('.arrow.right');
+    let current = 0;
+    let interval;
+
+    function showImage(index) {
+        images.forEach(img => img.classList.remove('active'));
+        images[index].classList.add('active');
+    }
+
+    function nextImage() {
+        current = (current + 1) % images.length;
+        showImage(current);
+    }
+
+    function prevImage() {
+        current = (current - 1 + images.length) % images.length;
+        showImage(current);
+    }
+
+    function startAutoSlide() {
+        interval = setInterval(nextImage, 5000);
+    }
+
+    leftBtn.addEventListener('click', () => {
+        prevImage();
+        clearInterval(interval);
+        startAutoSlide();
+    });
+
+    rightBtn.addEventListener('click', () => {
+        nextImage();
+        clearInterval(interval);
+        startAutoSlide();
+    });
+
+    startAutoSlide();
+});
+
+//função para definir o link de contato seção produtos
+
+document.addEventListener('DOMContentLoaded', function () {
+    const link = document.getElementById('link-contato');
+
+    // Número do WhatsApp e de ligação
+    const numeroWhatsApp = '5521990364744';
+    const numeroTelefone = '+5521990364744';
+
+    // Detecta se é dispositivo móvel
+    const isMobile = /iPhone|iPad|iPod|Android|webOS|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+    if (isMobile) {
+        link.setAttribute('href', `tel:${numeroTelefone}`);
+    } else {
+        link.setAttribute('href', `https://wa.me/${numeroWhatsApp}`);
+    }
 });
 
